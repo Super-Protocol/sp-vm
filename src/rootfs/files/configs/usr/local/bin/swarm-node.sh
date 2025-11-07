9c435d0745b4449787023606689b51d42234f8bb
Lex Kheben <i@zbit.name>
2025-11-07 21:06:14 +0300

    Update swarm-node.sh to manage WireGuard directory and create symbolic link for improved service configuration

--- src/rootfs/files/configs/usr/local/bin/swarm-node.sh
+++ src/rootfs/files/configs/usr/local/bin/swarm-node.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 
 cd /etc/swarm-cloud
 cp -fR /etc/swarm-cloud/services/ /var/lib/swarm-svc/
+rmdir /etc/wireguard
+mkdir /var/lib/etc-wireguard
+ln -s /var/lib/etc-wireguard /etc/wireguard
 
 exec node ./apps/swarm-node/dist/main.js
