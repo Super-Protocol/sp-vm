b5a4a60c83f117236bb0d2310fc23e659f070ecb
Lex Kheben <i@zbit.name>
2025-11-07 21:07:45 +0300

    Refactor swarm-node.sh to remove WireGuard directory and create symbolic link in Dockerfile for improved configuration management

--- src/rootfs/files/configs/usr/local/bin/swarm-node.sh
+++ src/rootfs/files/configs/usr/local/bin/swarm-node.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 cd /etc/swarm-cloud
 cp -fR /etc/swarm-cloud/services/ /var/lib/swarm-svc/
-rmdir /etc/wireguard
 mkdir /var/lib/etc-wireguard
-ln -s /var/lib/etc-wireguard /etc/wireguard
 
 exec node ./apps/swarm-node/dist/main.js
