4c79ec7c312ce2a6d65fb48b9cc0cacacbd312de
Lex Kheben <i@zbit.name>
2025-11-12 17:55:45 +0300

    Update swarm-node.sh to create Rancher directory and adjust Dockerfile for consistent directory setup

--- src/rootfs/files/configs/usr/local/bin/swarm-node.sh
+++ src/rootfs/files/configs/usr/local/bin/swarm-node.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 cd /etc/swarm-cloud
 cp -fR /etc/swarm-cloud/services/ /var/lib/swarm-svc/
 mkdir /var/lib/etc-wireguard
+mkdir /var/lib/etc-rancher
 
 exec node ./apps/swarm-node/dist/main.js
