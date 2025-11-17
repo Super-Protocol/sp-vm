a3af7fc7b69031110a31ec64dc36654271cf139e
Lex Kheben <i@zbit.name>
2025-11-17 14:32:04 +0300

    Remove unnecessary directory creation commands in swarm-node.sh to streamline the service initialization process and improve script clarity.

--- src/rootfs/files/configs/usr/local/bin/swarm-node.sh
+++ src/rootfs/files/configs/usr/local/bin/swarm-node.sh
@@ -3,8 +3,5 @@
 set -euo pipefail
 
 cd /etc/swarm-cloud
-cp -fR /etc/swarm-cloud/services/ /var/lib/swarm-svc/
-mkdir /var/lib/etc-wireguard
-mkdir -p /var/lib/etc-rancher/rke2
 
 exec node ./apps/swarm-node/dist/main.js
