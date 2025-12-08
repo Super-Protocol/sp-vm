eb7cf665366bf2fb8e49932ad83fd259db50f379
DrunkBatya <drunkbatya.js@gmail.com>
2025-12-08 02:19:41 -0600

    added logs handlers and mounting RO provider config

--- src/initramfs/files/init.sh
+++ src/initramfs/files/init.sh
@@ -32,7 +32,7 @@ get_device() {
 }
 
 _log() {
-    echo "$1: $0: $2" > /dev/console 2>&1
+    echo -e "$1: $0: $2" > /dev/console 2>&1
 }
 
 log_fail() {
@@ -49,7 +49,7 @@ log_info() {
     _log "INFO" "$1";
 }
 
-log_info "starting SP init";
+log_info "Starting SP init";
 
 rootfs_verifier="$(get_option rootfs_verity.scheme)";
 rootfs_hash="$(get_option rootfs_verity.hash)";
@@ -58,74 +58,82 @@ root_device_name="$(get_option root)";
 root_device="$(get_device "$root_device_name")";
 
 # hash device can only be found by partition label (not fs label)
-hash_device="$(blkid -t PARTLABEL="rootfs_hash" --output device || echo)";
+hash_device_path="$(blkid -t PARTLABEL="rootfs_hash" --output device || echo)";
 
-provider_config_device="$(blkid -L provider_config --output device)";
+provider_config_device_path="$(blkid -L provider_config --output device || echo)";
 
 # The root device should exist to be either verified then mounted or
 # just mounted when verification is disabled.
 if [ ! -e "${root_device}" ]; then
-    log_fail "No root device ${root_device} found"
+    log_fail "No root device ${root_device} found";
 fi
-if [ ! -e "${provider_config_device}" ]; then
-    log_fail "No root device ${provider_config_device} found"
+if [ ! -e "${provider_config_device_path}" ]; then
+    log_fail "No root device ${provider_config_device_path} found";
 fi
 
 if [ "${rootfs_verifier}" = "dm-verity" ]; then
-    log_info "Verify the root device with ${rootfs_verifier}"
+    log_info "Verify the root device with ${rootfs_verifier}";
 
-    if [ ! -e "${hash_device}" ]; then
-        log_fail "No hash device ${hash_device} found. Cannot verify the root device"
+    if [ ! -e "${hash_device_path}" ]; then
+        log_fail "No hash device ${hash_device_path} found. Cannot verify the root device";
     fi
 
-    log_info "Verifying rootfs RO hash"
-    veritysetup open "${root_device}" root "${hash_device}" "${rootfs_hash}"
-    log_info "Mounting rootfs RO"
-    mount /dev/mapper/root /sysroot-ro
+    log_info "Verifying rootfs RO hash";
+    veritysetup open "${root_device}" root "${hash_device_path}" "${rootfs_hash}" || log_fail "Verifying failed";
+    log_info "Mounting rootfs RO";
+    mount /dev/mapper/root /sysroot-ro || log_fail "Mounting failed";
 else
-    log_warn "Skipping rootfs RO hash check"
-    log_info "Mounting rootfs RO"
-    mount "${root_device}" /sysroot-ro
+    log_warn "Skipping rootfs RO hash check";
+    log_info "Mounting rootfs RO";
+    mount "${root_device}" /sysroot-ro || log_fail "Mounting failed";
 fi
 
-log_info "$(lsblk)";
+log_info "All avaliable block devicts:\n$(lsblk)";
 
 main_block_device_name="$(lsblk -no PKNAME "$root_device" | grep -v "$(basename "$root_device")")";
 if [ -z "$main_block_device_name" ]; then
     log_fail "Failed to get main block device name from data part device path '$root_device'..";
 fi
 
-state_block_device_name="$(lsblk -d -n -o NAME | grep -v "$main_block_device_name" | grep -v "$(basename "$provider_config_device")")";
+state_block_device_name="$(lsblk -d -n -o NAME | grep -v "$main_block_device_name" | grep -v "$(basename "$provider_config_device_path")")";
 if [ -z "$state_block_device_name" ]; then
-    log_fail "Failed to get state block device, this error is abnomal and you should notify the SuperProtocol support team if you see this.."
+    log_fail "Failed to get state block device, this error is abnomal and you should notify the SuperProtocol support team if you see this..";
 fi
 state_block_device_path="/dev/$state_block_device_name";
 
-echo -e "TRACE: root_device = $root_device\n" > /dev/console
-echo -e "TRACE: provider_config_device = $provider_config_device\n" > /dev/console
-echo -e "TRACE: state_block_device_path = $state_block_device_path\n" > /dev/console
 
 # Mounting encrypted state disk
+log_info "Mounting encrypted state disk";
+
 random_key="$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64)";
-echo -e "TRACE: wipefs -a \$state_block_device_path output:" > /dev/console
-wipefs -a "$state_block_device_path" >/dev/console 2>&1 || true;
-echo -e "\nTRACE: cryptsetup luksFormat \$state_block_device_path output:" > /dev/console
-echo "\$random_key" | cryptsetup luksFormat "$state_block_device_path" --batch-mode >/dev/console 2>&1
-echo -e "\nTRACE: cryptsetup luksOpen \$state_block_device_path output:" > /dev/console
-echo "\$random_key" | cryptsetup luksOpen "$state_block_device_path" crypto >/dev/console 2>&1
-echo -e "\nTRACE: mkfs.ext4 /dev/mapper/crypto output:" > /dev/console
-mkfs.ext4 /dev/mapper/crypto >/dev/console 2>&1
-echo -e "\nTRACE: mount /dev/mapper/crypto /sysroot-rw output:" > /dev/console
-mount /dev/mapper/crypto /sysroot-rw >/dev/console 2>&1
+
+wipefs -a "$state_block_device_path" || log_warn "Failed to wipe filesystem signatures from the device $state_block_device_path";
+
+echo "$random_key" | cryptsetup luksFormat "$state_block_device_path" --batch-mode || log_fail "Failed to format the device $state_block_device_path as LUKS encrypted";
+echo "$random_key" | cryptsetup luksOpen "$state_block_device_path" crypto || log_fail "Failed to open the LUKS encrypted device $state_block_device_path";
+
+mkfs.ext4 /dev/mapper/crypto || log_fail "Failed to create ext4 filesystem on the /dev/mapper/crypto";
+
+mount /dev/mapper/crypto /sysroot-rw || log_fail "Mounting failed";
+
+log_info "Mounting overlay filesystem";
 
 [ -d /sysroot-rw/upper ] || mkdir /sysroot-rw/upper
 [ -d /sysroot-rw/work ] || mkdir /sysroot-rw/work
 
-echo -e "\nTRACE: mount overlay output:" > /dev/console
 mount -t overlay overlay \
   -o lowerdir=/sysroot-ro,upperdir=/sysroot-rw/upper,workdir=/sysroot-rw/work \
-  /mnt >/dev/console 2>&1
+  /mnt || log_fail "Mounting failed";
+
+# Mounting read-only provider config
+log_info "Mounting RO provider config";
+
+[ -d /mnt/sp ] || mkdir /mnt/sp
+
+mount -t ext4 -o ro "$provider_config_device_path" /mnt/sp || log_fail "Mounting failed";
 
 umount /proc
 umount /sys
+
+log_info "Starting true real respectable init! Bye..";
 exec switch_root /mnt /sbin/init
