888ec14693916f799675aafc91e6127bb8531e95
Lex Kheben <i@zbit.name>
2026-07-02 14:24:12 +0300

    feat: add KERNEL_SHA256 argument to Dockerfile and update checksum handling in download script
    Introduced a new KERNEL_SHA256 argument in the Dockerfile for kernel source verification. Updated the download_and_verify_kernel_src.sh script to utilize this argument for preparing kernel checksums, enhancing the integrity verification process.

--- src/kernel/files/scripts/download_and_verify_kernel_src.sh
+++ src/kernel/files/scripts/download_and_verify_kernel_src.sh
@@ -5,6 +5,7 @@ set -euo pipefail;
 
 # public, required
 # KERNEL_VERSION
+# KERNEL_SHA256
 
 # private
 BUILDROOT="/buildroot";
@@ -29,10 +30,10 @@ function download_kernel() {
 }
 
 function download_kernel_checksums() {
-    log_info "downloading kernel checksums"
-    echo "191dc5aae14a4223f0d5ce4e88cbbd29f0377703b9f0b70d4903734de641fb6a  linux-$KERNEL_VERSION.tar.gz" \
+    log_info "preparing kernel checksums"
+    echo "$KERNEL_SHA256  linux-$KERNEL_VERSION.tar.gz" \
         > "$BUILDROOT/src/linux-$KERNEL_VERSION.sha256" \
-        || log_fail "failed to download kernel checksums";
+        || log_fail "failed to prepare kernel checksums";
 }
 
 function verify_kernel_checksum() {
